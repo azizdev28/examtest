bdd3058247476da1f45b34dde96f66b23c4ec151
Aziz28 <az1z.bakht1yarvich@gmail.com>
2024-03-19 20:21:25 +0500

    add

--- src/pages/AddProducts.tsx
+++ src/pages/AddProducts.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, FileInput, TextInput } from "flowbite-react";
 import {
